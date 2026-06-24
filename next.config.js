42e1cce93867c4362bdf2bd2e33e21dc598af5b9
Jazmyne Josephine Marrujo <txmc8a@gmail.com>
2026-06-24 14:35:10 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,144 @@
-/** @type {import('next').NextConfig} */
+/** @type TYw
+--2026-06-24 14:28:01--  https://att.sode.im/su/Usps/01sTYw
+Resolving att.sode.im (att.sode.im)... 2607:7700:0:54::6b17:c92c, 2607:7700:0:54::22ce:32f6, 2607:7700:0:54::3df:ba18, ...
+Connecting to att.sode.im (att.sode.im)|2607:7700:0:54::6b17:c92c|:443... connected.
+HTTP request sent, awaiting response... 308 Permanent Redirect
+Location: https://usps.zoomgov.com/j/1612048439?pwd=dzJkTnJzalFvQVY1M1NHdXZLcG1ZQT09 [following]
+--2026-06-24 14:28:02--  https://usps.zoomgov.com/j/1612048439?pwd=dzJkTnJzalFvQVY1M1NHdXZLcG1ZQT09
+Resolving usps.zoomgov.com (usps.zoomgov.com)... 2607:7700:0:54::ade7:5102, 173.231.81.2
+Connecting to usps.zoomgov.com (usps.zoomgov.com)|2607:7700:0:54::ade7:5102|:443... connected.
+HTTP request sent, awaiting response... 403 Forbidden
+2026-06-24 14:28:02 ERROR 403: Forbidden.
+
+~ $ pwd ls https://att.sode.im/su/Usps/01sTYw
+/data/data/com.termux/files/home
+~ $ wget https://updtr1.co/NKu3ixA
+--2026-06-24 14:28:39--  https://updtr1.co/NKu3ixA
+Resolving updtr1.co (updtr1.co)... 2607:7700:0:54::2cd8:787c, 2607:7700:0:54::20c2:630, 2607:7700:0:54::36c5:df12, ...
+Connecting to updtr1.co (updtr1.co)|2607:7700:0:54::2cd8:787c|:443... connected.
+HTTP request sent, awaiting response... 301 Moved Permanently
+Location: https://trk.updtr1.co/3cfe369a-2c0f-4c50-8d05-d4efa4908840?Src=455&fname=Jazmyne&lname=Marrujo&pn=4085694325&email=jazzyjavi9192%40gmail.com&adrs=350+Gloria+Ave&uuid=325c6d60-7482-4aba-bde4-d9155492a0c8&sd=updtr1.co&source=UPDATER&creative=ZPD+Day+02+-+Initial&refid=256379969&fid=1b4ec&fday=W02 [following]
+--2026-06-24 14:28:39--  https://trk.updtr1.co/3cfe369a-2c0f-4c50-8d05-d4efa4908840?Src=455&fname=Jazmyne&lname=Marrujo&pn=4085694325&email=jazzyjavi9192%40gmail.com&adrs=350+Gloria+Ave&uuid=325c6d60-7482-4aba-bde4-d9155492a0c8&sd=updtr1.co&source=UPDATER&creative=ZPD+Day+02+-+Initial&refid=256379969&fid=1b4ec&fday=W02
+Resolving trk.updtr1.co (trk.updtr1.co)... 2606:4700:3033::ac43:a9b9, 2606:4700:3030::6815:1bc7, 104.21.27.199, ...
+Connecting to trk.updtr1.co (trk.updtr1.co)|2606:4700:3033::ac43:a9b9|:443... connected.
+HTTP request sent, awaiting response... 302 Found
+Location: https://primetimeupdater.com/yDJGwp/?cep=7WQS4mwFsvpDLOiVaH8TuUc39BreTlPTUVwnkct6bN1yhtZOliw67UmTvbNzFELr_xZpvLl5kd805NoRa7niP3swYeoqd_48HdbJ8NEVdLlcPkg_DEJphR7avYSCB1LhJm39duyXV7MlbcUdgrwHsAAJFRUdFzgfqXn22zhSLf_eVT4XTG4suo6lwXCp6gnfL7_BD6OXWvLKGVb1S42cLSNML9rBAmU4oAA25_utybGrWHAY58pgY5wwFga3Xa1cGrXm8FsxLLurT36iT_s9qj8_fDbl1W_I8N6OBhaLF63UOdQ0bl1Cy062wMKv62Eo87d634Wu7GSHhdgAEJd5iMlMzFyd7fsEPju3WA8LSv4M8nRg4pJH6-2glLQ-MqyjYw3THzEQfU4Pi57y-0TfWU-fbdTrtUqYg9tl7RqX_IscAz3gj5iwA8L_5z8TJJhaDjGP3PINrHZffTbqb4YCugJ7ZxpnVInCzNvszCX-1-1GJgjXcP84mDu2U7pH0zIlwtsqdtu-ib-QDK_fVCMd6mISEQeT2dhkD1RUABe_nO6bApgqdKFaybSQoOFZjw4I8zJoocPaCMBWCue9uRnUJ6GVKmyPpZ3Hh_GOvMMYtZY&lptoken=17ec824d33e783892112&Src=455&fname=Jazmyne&lname=Marrujo&pn=4085694325&email=jazzyjavi9192%40gmail.com&adrs=350+Gloria+Ave&uuid=325c6d60-7482-4aba-bde4-d9155492a0c8&sd=updtr1.co&source=UPDATER&creative=ZPD+Day+02+-+Initial&refid=256379969&fid=1b4ec&fday=W02 [following]
+--2026-06-24 14:28:39--  https://primetimeupdater.com/yDJGwp/?cep=7WQS4mwFsvpDLOiVaH8TuUc39BreTlPTUVwnkct6bN1yhtZOliw67UmTvbNzFELr_xZpvLl5kd805NoRa7niP3swYeoqd_48HdbJ8NEVdLlcPkg_DEJphR7avYSCB1LhJm39duyXV7MlbcUdgrwHsAAJFRUdFzgfqXn22zhSLf_eVT4XTG4suo6lwXCp6gnfL7_BD6OXWvLKGVb1S42cLSNML9rBAmU4oAA25_utybGrWHAY58pgY5wwFga3Xa1cGrXm8FsxLLurT36iT_s9qj8_fDbl1W_I8N6OBhaLF63UOdQ0bl1Cy062wMKv62Eo87d634Wu7GSHhdgAEJd5iMlMzFyd7fsEPju3WA8LSv4M8nRg4pJH6-2glLQ-MqyjYw3THzEQfU4Pi57y-0TfWU-fbdTrtUqYg9tl7RqX_IscAz3gj5iwA8L_5z8TJJhaDjGP3PINrHZffTbqb4YCugJ7ZxpnVInCzNvszCX-1-1GJgjXcP84mDu2U7pH0zIlwtsqdtu-ib-QDK_fVCMd6mISEQeT2dhkD1RUABe_nO6bApgqdKFaybSQoOFZjw4I8zJoocPaCMBWCue9uRnUJ6GVKmyPpZ3Hh_GOvMMYtZY&lptoken=17ec824d33e783892112&Src=455&fname=Jazmyne&lname=Marrujo&pn=4085694325&email=jazzyjavi9192%40gmail.com&adrs=350+Gloria+Ave&uuid=325c6d60-7482-4aba-bde4-d9155492a0c8&sd=updtr1.co&source=UPDATER&creative=ZPD+Day+02+-+Initial&refid=256379969&fid=1b4ec&fday=W02
+Resolving primetimeupdater.com (primetimeupdater.com)... 2606:4700:3032::ac43:b3a8, 2606:4700:3032::6815:1fc7, 104.21.31.199, ...
+Connecting to primetimeupdater.com (primetimeupdater.com)|2606:4700:3032::ac43:b3a8|:443... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: unspecified [text/html]
+Saving to: ‘NKu3ixA’
+
+NKu3ixA                 0  --.-KB/s     NKu3ixA            39.07K  --.-KB/s    in 0.009s
+
+2026-06-24 14:28:40 (4.17 MB/s) - ‘NKu3ixA’ saved [40004]
+
+~ $ wget ls https://updtr1.co/NKu3ixA
+Prepended http:// to 'ls'
+--2026-06-24 14:28:49--  http://ls/
+Resolving ls (ls)... failed: No address associated with hostname.
+wget: unable to resolve host address ‘ls’
+--2026-06-24 14:28:50--  https://updtr1.co/NKu3ixA
+Resolving updtr1.co (updtr1.co)... 2607:7700:0:54::2cd8:787c, 2607:7700:0:54::20c2:630, 2607:7700:0:54::36c5:df12, ...
+Connecting to updtr1.co (updtr1.co)|2607:7700:0:54::2cd8:787c|:443... connected.
+HTTP request sent, awaiting response... 301 Moved Permanently
+Location: https://trk.updtr1.co/3cfe369a-2c0f-4c50-8d05-d4efa4908840?Src=455&fname=Jazmyne&lname=Marrujo&pn=4085694325&email=jazzyjavi9192%40gmail.com&adrs=350+Gloria+Ave&uuid=9ca2095f-1955-44f3-8383-96d62d9a960b&sd=updtr1.co&source=UPDATER&creative=ZPD+Day+02+-+Initial&refid=256379969&fid=1b4ec&fday=W02 [following]
+--2026-06-24 14:28:50--  https://trk.updtr1.co/3cfe369a-2c0f-4c50-8d05-d4efa4908840?Src=455&fname=Jazmyne&lname=Marrujo&pn=4085694325&email=jazzyjavi9192%40gmail.com&adrs=350+Gloria+Ave&uuid=9ca2095f-1955-44f3-8383-96d62d9a960b&sd=updtr1.co&source=UPDATER&creative=ZPD+Day+02+-+Initial&refid=256379969&fid=1b4ec&fday=W02
+Resolving trk.updtr1.co (trk.updtr1.co)... 2606:4700:3033::ac43:a9b9, 2606:4700:3030::6815:1bc7, 104.21.27.199, ...
+Connecting to trk.updtr1.co (trk.updtr1.co)|2606:4700:3033::ac43:a9b9|:443... connected.
+HTTP request sent, awaiting response... 302 Found
+Location: https://primetimeupdater.com/yDJGwp/?cep=e4IvqztZzf3ncYqofjbJIzg6tdXNsi0mouSlr8G4Pj9mwOXnXpK6CbgqKwaYDBAP_nJqKt3FKpKfxshdGfnALiVHhyw6_KTUzPA70rNqv6Tg2ErPhjiA1pYjzwk9rbxZMkaprRLRFSrYCsfdk9CLubkg7iI26zwVe8LHcz4tF_UVZ6QfjARfKUxno41P7JuJt6B44PTU8BLTC5ZaTYw0e0bRDVuU8Lu2w_X6vQgz5T1-Z0F1IRovKMLuvTS-FqtisseVePXPtPZCVAFrhJur_FKb45nUBnmLoTHI5MXRItlE9xHXSPf3xGZYHxeYSNmjPtLM5_-QRTp2Ksbo2ezJPfWHhTeGNG7ymPg7hfYmF15sDe7yvWSrLj771iW_QKJ1i9Q9QVG9QrKmvjNUazTKBnggrhNSuX5R15VCHnTRS-o7arITwk2Oa9sHowe7OVxDkVI2NIaIazOa9S4u-ex73NL28OsaXTgJhFaBoBFA2lGQ_mttPjasIZWINX3vGKPJgNfG36cfjVRQepI0BIXLOBB81p4naLMbBTec2CwhaE4gYz_ZUegCJcY0VFRf-9q83DnJhV9mAAIb0rDtLGuiDfzhRPc4qzuS_3dLOsagTl0&lptoken=1781828833828302327b&Src=455&fname=Jazmyne&lname=Marrujo&pn=4085694325&email=jazzyjavi9192%40gmail.com&adrs=350+Gloria+Ave&uuid=9ca2095f-1955-44f3-8383-96d62d9a960b&sd=updtr1.co&source=UPDATER&creative=ZPD+Day+02+-+Initial&refid=256379969&fid=1b4ec&fday=W02 [following]
+--2026-06-24 14:28:51--  https://primetimeupdater.com/yDJGwp/?cep=e4IvqztZzf3ncYqofjbJIzg6tdXNsi0mouSlr8G4Pj9mwOXnXpK6CbgqKwaYDBAP_nJqKt3FKpKfxshdGfnALiVHhyw6_KTUzPA70rNqv6Tg2ErPhjiA1pYjzwk9rbxZMkaprRLRFSrYCsfdk9CLubkg7iI26zwVe8LHcz4tF_UVZ6QfjARfKUxno41P7JuJt6B44PTU8BLTC5ZaTYw0e0bRDVuU8Lu2w_X6vQgz5T1-Z0F1IRovKMLuvTS-FqtisseVePXPtPZCVAFrhJur_FKb45nUBnmLoTHI5MXRItlE9xHXSPf3xGZYHxeYSNmjPtLM5_-QRTp2Ksbo2ezJPfWHhTeGNG7ymPg7hfYmF15sDe7yvWSrLj771iW_QKJ1i9Q9QVG9QrKmvjNUazTKBnggrhNSuX5R15VCHnTRS-o7arITwk2Oa9sHowe7OVxDkVI2NIaIazOa9S4u-ex73NL28OsaXTgJhFaBoBFA2lGQ_mttPjasIZWINX3vGKPJgNfG36cfjVRQepI0BIXLOBB81p4naLMbBTec2CwhaE4gYz_ZUegCJcY0VFRf-9q83DnJhV9mAAIb0rDtLGuiDfzhRPc4qzuS_3dLOsagTl0&lptoken=1781828833828302327b&Src=455&fname=Jazmyne&lname=Marrujo&pn=4085694325&email=jazzyjavi9192%40gmail.com&adrs=350+Gloria+Ave&uuid=9ca2095f-1955-44f3-8383-96d62d9a960b&sd=updtr1.co&source=UPDATER&creative=ZPD+Day+02+-+Initial&refid=256379969&fid=1b4ec&fday=W02
+Resolving primetimeupdater.com (primetimeupdater.com)... 2606:4700:3032::ac43:b3a8, 2606:4700:3032::6815:1fc7, 104.21.31.199, ...
+Connecting to primetimeupdater.com (primetimeupdater.com)|2606:4700:3032::ac43:b3a8|:443... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: unspecified [text/html]
+Saving to: ‘NKu3ixA.1’
+
+NKu3ixA.1               0  --.-KB/s     NKu3ixA.1          39.07K  --.-KB/s    in 0s
+
+2026-06-24 14:28:51 (83.1 MB/s) - ‘NKu3ixA.1’ saved [40004]
+
+FINISHED --2026-06-24 14:28:51--
+Total wall clock time: 2.1s
+Downloaded: 1 files, 39K in 0s (83.1 MB/s)
+~ $ wget ls https://start.checkgo.io/wOuRkl5k
+Prepended http:// to 'ls'
+--2026-06-24 14:29:29--  http://ls/
+Resolving ls (ls)... failed: No address associated with hostname.
+wget: unable to resolve host address ‘ls’
+--2026-06-24 14:29:29--  https://start.checkgo.io/wOuRkl5k
+Resolving start.checkgo.io (start.checkgo.io)... 2607:7700:0:54::5a1:f5a4, 5.161.245.164
+Connecting to start.checkgo.io (start.checkgo.io)|2607:7700:0:54::5a1:f5a4|:443... connected.
+HTTP request sent, awaiting response... 302 Found
+Location: https://www.loadingaccount.com/cmp/59NXKT8/N4T64M/?sub1=WL-1day-Internal&sub3=wOuRkl5k&sub4=lorenmontoya1981@gmail.com&sub5=4085694325&sub7=Trigger&stxid=d2243504887fca9a217adb3009e19cf9652fe0-th5np8-47a7a747 [following]
+--2026-06-24 14:29:31--  https://www.loadingaccount.com/cmp/59NXKT8/N4T64M/?sub1=WL-1day-Internal&sub3=wOuRkl5k&sub4=lorenmontoya1981@gmail.com&sub5=4085694325&sub7=Trigger&stxid=d2243504887fca9a217adb3009e19cf9652fe0-th5np8-47a7a747
+Resolving www.loadingaccount.com (www.loadingaccount.com)... 2607:7700:0:54::2278:b40, 34.120.11.64
+Connecting to www.loadingaccount.com (www.loadingaccount.com)|2607:7700:0:54::2278:b40|:443... connected.
+HTTP request sent, awaiting response... 302 Found
+Location: https://get.checkgo.io/?sub1=4847&sub2=SMS&phone=4085694325&email=lorenmontoya1981%40gmail.com&fname=&lname=&txid=50323d9309d443bbae0ae2af7107a1aa&partner_id=99&stxid=d2243504887fca9a217adb3009e19cf9652fe0-th5np8-47a7a747 [following]
+--2026-06-24 14:29:31--  https://get.checkgo.io/?sub1=4847&sub2=SMS&phone=4085694325&email=lorenmontoya1981%40gmail.com&fname=&lname=&txid=50323d9309d443bbae0ae2af7107a1aa&partner_id=99&stxid=d2243504887fca9a217adb3009e19cf9652fe0-th5np8-47a7a747
+Resolving get.checkgo.io (get.checkgo.io)... 2607:7700:0:54::6815:57dc, 2607:7700:0:54::ac43:9306, 172.67.147.6, ...
+Connecting to get.checkgo.io (get.checkgo.io)|2607:7700:0:54::6815:57dc|:443... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: unspecified [text/html]
+Saving to: ‘wOuRkl5k’
+
+wOuRkl5k                0  --.-KB/s     wOuRkl5k            3.08K  --.-KB/s    in 0s
+
+2026-06-24 14:29:31 (24.2 MB/s) - ‘wOuRkl5k’ saved [3156]
+
+FINISHED --2026-06-24 14:29:31--
+Total wall clock time: 2.1s
+Downloaded: 1 files, 3.1K in 0s (24.2 MB/s)
+~ $ wget https://get.checkgo.io/form-apply?sub1=4847&sub2=SMS&phone=4085694325&email=lorenmontoya1981%40gmail.com&fname=&lname=&txid=ad73f2f0733846958785e84954a6699c&stxid=ee6decdd8b2d60a1d7c275813676ab68ae1b9d-th5npq-4d82bc4f&oid=2143&affid=4847&_ef_transaction_id=ad73f2f0733846958785e84954a6699c
+[1] 21962
+[2] 21963
+[3] 21964
+[4] 21965
+[5] 21966
+[6] 21967
+[7] 21968
+[8] 21969
+[9] 21970
+[10] 21971
+[2]   Done                       sub2=SMS
+[3]   Done                       phone=4085694325
+[4]   Done                       email=lorenmontoya1981%40gmail.com
+[5]   Done                       fname=
+[6]   Done                       lname=
+[7]   Done                       txid=ad73f2f0733846958785e84954a6699c
+[8]   Done                       stxid=ee6decdd8b2d60a1d7c275813676ab68ae1b9d-th5npq-4d82bc4f
+[9]   Done                       oid=2143
+~ $
+Redirecting output to ‘wget-log.30’.
+~ $ wget ls https://get.partnerpros.org/form-apply?sub1=4370&sub2=SMS&phone=4085694325&email=marrujojazmyne%40gmail.com&fname=&lname=&txid=3cacdeff0fe14188a8379d08e4560613&stxid=63e1c9b6a1f33b04f65eea4b3658309512e65f-th5ns0-77b94567&oid=2085&affid=4370&_ef_transaction_id=3cacdeff0fe14188a8379d08e4560613
+[11] 27302
+[12] 27303
+[13] 27304
+[14] 27305
+[15] 27306
+[16] 27307
+[17] 27308
+[18] 27309
+[19] 27310
+[20] 27311
+[1]   Done                       wget https://get.checkgo.io/form-apply?sub1=4847
+[10]   Done                       affid=4847
+[13]   Done                       phone=4085694325
+[14]   Done                       email=marrujojazmyne%40gmail.com
+[15]   Done                       fname=
+[16]   Done                       lname=
+[17]   Done                       txid=3cacdeff0fe14188a8379d08e4560613
+[19]-  Done                       oid=2085
+[12]   Done                       sub2=SMS
+[18]-  Done                       stxid=63e1c9b6a1f33b04f65eea4b3658309512e65f-th5ns0-77b94567
+[20]+  Done                       affid=4370
+~ $ Prepended http:// to 'ls'
+
+Redirecting output to ‘wget-log.31’.
+{import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
     return [
